cad8a93e156913dff9c6607cd9b53110d3f0718b
alma tamir <alma.tamir@runi.ac.il>
2024-01-13 19:35:13 +0300

    g

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,90 @@
 public class ArrayOps {
     public static void main(String[] args) {
+        int[] numbers = {9, 8, 7};
+        System.out.println(findMissingInt(numbers));
+        System.out.println(secondMaxValue(numbers));
+        System.out.println(isSorted(numbers));
         
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int sum= 0 ;
+        int count = 0;
+        for (int i=0; i<array.length;i++)
+        {
+            sum+=i;
+        }
+        sum+=array.length;
+        for(int i=0; i< array.length;i++)
+        {
+            count+=array[i];
+        }
+        sum = sum - count;
+        return sum;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int max =0; 
+        for (int i=0;i<array.length;i++)
+        {
+            if (max<array[i])
+            max = array[i];
+        }
+        int newmax =0;
+        for (int i=0;i<array.length;i++)
+        {
+            if (array[i]== max)
+            i++;
+            else if (newmax < array[i])
+            {
+            newmax = array[i];
+            }
+        }
+        return newmax;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
+        int count=0;
+        for (int i=0; i<array1.length; i++)
+        {
+            for ( int j=0; j<array2.length;j++)
+            {
+                if(array1[i]==array2[j])
+                {
+                count++;
+                j=array2.length;
+                }
+
+            }
+            if (count >= array2.length)
+            return true;
+        }
         return false;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        int keep = array[0];
+        if(keep <= array[1])
+        {
+            for(int i=1;i<array.length;i++)
+            {
+                keep = array [i];
+                if (!(keep <= array[i+1]))
+                return false;
+            }
+            
+        }
+        else if(keep >= array[1])
+        {
+            for(int i=1;i<array.length;i++)
+            {
+                keep = array [i];
+                if (!(keep >= array[i+1]))
+                return false;
+            }
+            
+        }
+        return true;
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,125 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
+        String str = args[0];
+        String ch = args[1];
+        char chr = ch.charAt(0);
+        System.out.println(capVowelsLowRest(str));
+        System.out.println(camelCase(str));
+        int arr[] = (allIndexOf(str, chr));
+        System.out.print("{");
+        for (int i=0; i< arr.length; i++)
+        {
+            System.out.print(arr[i]+",");
+            if (i+1==arr.length)
+            System.out.print(arr[i]+"}");
+            
+        }
+        
+        
         
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String news = "";
+        String vowelsC ="AEUIO";
+        String vowels ="aeuio";
+        for (int i=0; i < string.length() ;i++)
+        {
+            char ch = string.charAt(i);
+            if ( ((ch >= 'A') && (ch <= 'Z'))) // if its capital
+            {
+                if (vowelsC.indexOf(ch) == -1) // check if ch is is in vowelsC, if it isnt it changes
+                {
+                    char lowercaseChar = (char) (ch + ('a' - 'A')); // turns to small letters
+                    news += lowercaseChar;
+                }
+                else if(vowelsC.indexOf(ch) != -1)
+                {
+                    news+= ch;
+                }
+            }
+            else if (vowels.indexOf(ch) != -1) // check if ch is lower letter vowel, if it is it changes
+                {
+                    char uppercaseChar = (char) (ch + ('A' - 'a')); // turns to capital letters
+                    news += uppercaseChar;
+                }
+            else news+= ch;
+        }
+        
+        return news;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String news =" ";
+        char ch = ' ';
+        String substring1 = "";
+        for (int j=0; j < string.length(); j++) 
+        {
+            if (string.charAt(j) != ' ')
+            {
+                substring1 = string.substring(j);
+                j=string.length();
+            }
+        }
+        ch = substring1.charAt(0);
+        if ( ((ch >= 'A') && (ch <= 'Z'))) //fixes first letter
+                {
+                    char lowercaseChar = (char) (ch + ('a' - 'A')); // turns to small letters
+                    news += lowercaseChar;
+                }
+                else news += ch;
+
+        
+       
+        for (int k = 1 ; k < substring1.length(); k++)
+        {
+            ch = substring1.charAt(k);
+            if (substring1.charAt(k)== ' ')
+            {
+                ch = substring1.charAt(k+1);
+                if ( ((ch >= 'A') && (ch <= 'Z'))) // if its capital
+                {
+                    news+= ch;
+                }
+                else 
+                {
+                   char uppercaseChar = (char) (ch + ('A' - 'a')); // turns to capital letters
+                   news += uppercaseChar;
+                   k++;
+                }
+            }
+            else if ( ((ch >= 'A') && (ch <= 'Z'))) // if its capital
+              {
+                char lowercaseChar = (char) (ch + ('a' - 'A')); // turns to small letters
+                news += lowercaseChar;
+              }
+             else news+= ch;                
+            
+            
+            
+        }
+        return news;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int countch = 0;
+        int place = 0;
+        for (int i=0; i < string.length(); i++)
+        {
+            if (string.charAt(i)==chr)
+            countch++;
+        }
+        int[] numbers = new int[countch];
+        for ( int j=0; j< string.length(); j++)
+        {
+            if (string.charAt(j)==chr)
+            {
+                 numbers[place]= j;
+                 place++;
+            }
+           
+        }
+        return numbers;
     }
 }
